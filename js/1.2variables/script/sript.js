81462a36ba3b3fc62b0536c8c6b0e74b28c68cf1
kanademruuu@gmail.com <kanademruuu@gmail.com>
2021-10-16 22:17:24 +0200

    variables js

---
+++ js/1.2variables/script/sript.js
@@ -0,0 +1,25 @@
+const box=document.getElementById('myBox');
+function psychoAnimate(){
+  
+        box.style=`
+        background-color:rgb(${Math.random()*250},${Math.random()*250},${Math.random()*250});
+        margin-top:${Math.random()*25}px;
+        `
+    
+}
+// psychoAnimate()
+
+box.addEventListener("mouseover", psychoAnimate)
+
+
+function changeClass(){
+  box.getAttribute('class').includes('newclass')
+  ? box.setAttribute('class','box')
+  : box.setAttribute('class','box newclass')
+// console.log(box.getAttribute('class'));
+}
+// psychoAnimate()
+
+box.addEventListener("click", changeClass)
+
+console.log(changeClass)
