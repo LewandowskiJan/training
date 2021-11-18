e95a8f5148b9a7d8339c0433c1dc5f637e2e0c70
kanademruuu@gmail.com <kanademruuu@gmail.com>
2021-11-18 16:56:22 +0100

    Round.service

---
+++ js/9.game/1.chess/script/round.service.js
@@ -0,0 +1,14 @@
+export default class RoundService {
+roundNumber;
+
+  constructor(roundNumber) {
+    this.roundNumber = roundNumber;
+  }
+
+  isPlayerOneRound() {
+    return x;
+  }
+  nextRound() {
+    return x;
+  }
+}
