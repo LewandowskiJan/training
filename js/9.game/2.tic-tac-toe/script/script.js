b82365a32dea16cbaf2b15b2ad00790a62ea908a
kanademruuu@gmail.com <kanademruuu@gmail.com>
2021-11-29 20:27:30 +0100

    changes in winCondition loop

--- js/9.game/2.tic-tac-toe/script/script.js
+++ js/9.game/2.tic-tac-toe/script/script.js
@@ -7,112 +7,98 @@ const winC6 = [3, 5, 7];
 const winC7 = [2, 5, 8];
 const winC8 = [4, 5, 6];
 
+winAllConditions = [
+  [1, 2, 3],
+  [3, 6, 9],
+  [7, 8, 9],
+  [1, 4, 7],
+  [1, 5, 9],
+  [3, 5, 7],
+  [2, 5, 8],
+  [4, 5, 6],
+];
+
 const clickOnBoard = document.getElementById('tictactoe');
-let round = 1
-let pushOpositionIdToArray = []
-let pushXpositionIdToArray = []
+let round = 1;
+let pushOpositionIdToArray = [];
+let pushXpositionIdToArray = [];
 
+clickOnBoard.addEventListener('click', (event) => {
+  let targetID = event.target.id;
 
-clickOnBoard.addEventListener('click', event => {
-    let targetID = event.target.id
- 
-    // console.log(event)
-    // console.log(event.target)
-    // console.log(event.target.childNodes)
-    // console.log(event.target.childNodes.length)
-    if (round % 2 === 0 && event.target.childNodes.length === 0) {
-        addO(targetID)
-        pushOpositionIdToArray.push(+event.target.id)
-        console.log(pushOpositionIdToArray)
-        console.log(winC1)
-        round++
-        if(pushOpositionIdToArray.length > 2 && 
-            winC1.every(r => pushOpositionIdToArray.includes(r)) ||
-            winC2.every(r => pushOpositionIdToArray.includes(r)) ||
-            winC3.every(r => pushOpositionIdToArray.includes(r)) ||
-            winC4.every(r => pushOpositionIdToArray.includes(r)) ||
-            winC5.every(r => pushOpositionIdToArray.includes(r)) ||
-            winC6.every(r => pushOpositionIdToArray.includes(r)) ||
-            winC7.every(r => pushOpositionIdToArray.includes(r)) ||
-            winC8.every(r => pushOpositionIdToArray.includes(r)))
-{
-            console.log('Found all of', pushOpositionIdToArray, 'in', winC1);
-            console.log("Player 0 won")
- 
-
-        }
+  if (round % 2 === 0 && event.target.childNodes.length === 0) {
+    addO(targetID);
+    pushOpositionIdToArray.push(+event.target.id);
+    console.log(pushOpositionIdToArray);
+    console.log(winC1);
+    round++;
+    if (
+      (pushOpositionIdToArray.length > 2 && winC1.every((r) => pushOpositionIdToArray.includes(r))) ||
+      winAllConditions.some(option => option.every(r => pushOpositionIdToArray.includes(r)))
+    //   winC2.every((r) => pushOpositionIdToArray.includes(r)) ||
+    //   winC3.every((r) => pushOpositionIdToArray.includes(r)) ||
+    //   winC4.every((r) => pushOpositionIdToArray.includes(r)) ||
+    //   winC5.every((r) => pushOpositionIdToArray.includes(r)) ||
+    //   winC6.every((r) => pushOpositionIdToArray.includes(r)) ||
+    //   winC7.every((r) => pushOpositionIdToArray.includes(r)) ||
+    //   winC8.every((r) => pushOpositionIdToArray.includes(r))
+    ) {
+      console.log('Found all of', pushOpositionIdToArray);
+      console.log('Player 0 won');
     }
-    else if (round % 2 !== 0 && event.target.childNodes.length === 0) {
-        addX(targetID)
-        pushXpositionIdToArray.push(+event.target.id)
-        console.log(pushXpositionIdToArray)
-        round++
-        if(pushXpositionIdToArray.length > 2 && 
-            winC1.every(r => pushXpositionIdToArray.includes(r)) ||
-            winC2.every(r => pushXpositionIdToArray.includes(r)) ||
-            winC3.every(r => pushXpositionIdToArray.includes(r)) ||
-            winC4.every(r => pushXpositionIdToArray.includes(r)) ||
-            winC5.every(r => pushXpositionIdToArray.includes(r)) ||
-            winC6.every(r => pushXpositionIdToArray.includes(r)) ||
-            winC7.every(r => pushXpositionIdToArray.includes(r)) ||
-            winC8.every(r => pushXpositionIdToArray.includes(r)))
-{
-            console.log('Found all of', pushXpositionIdToArray, 'in', winC1);
-            console.log("Player X won")
-    }}
+  } else if (round % 2 !== 0 && event.target.childNodes.length === 0) {
+    addX(targetID);
+    pushXpositionIdToArray.push(+event.target.id);
+    console.log(pushXpositionIdToArray);
+    round++;
+    if (
+      (pushXpositionIdToArray.length > 2 && winC1.every((r) => pushXpositionIdToArray.includes(r))) ||
+      winAllConditions.some((option) => option.every((r) => pushXpositionIdToArray.includes(r)))
+    ) {
+      console.log('Found all of', pushXpositionIdToArray);
+      console.log('Player X won');
+    }
+  }
+});
 
 
 
-   
-
-   
-
-   
-
-    // for (let i = 0; i < 9; i++) {
-    //     if (target.id = [i] && target.childNodes[0] === "O"){pushXpositionIdToArray.push(target.id)}
-
-
-
-//     for (let i = 0; i < event.target.id; i++) {
-//        if (event.target.id[i].childNodes === "O"){
-//         pushOpositionIdToArray.push(event.target.id[i])}
-//         else if (event.target.id[i].childNodes === "X"){
-//             pushXpositionIdToArray.push(event.target.id[i])
-//        }
-//        console.log(pushXpositionIdToArray)
-//        console.log(pushOpositionIdToArray)
+// const arr = [];
+// const arr2 = new Array();
+// class List {
+//   data;
+//   constructor(data) {
+//     this.data = data;
+//   }
+//   everyAisB(callback) {
+//     return this.data.split('').map(callback).join('');
+//   }
 // }
-})
-
-
-
-
-
+// const list = new List('abcd');
+// const xqwe = list.everyAisB((letter) => (letter === 'a' ? 'b' : letter));
+// console.log(xqwe);
 
 function addX(clickOnBoard) {
-    const cell = document.getElementById(clickOnBoard);
-    cell.innerHTML = ("&#10005;")
-    return
+  const cell = document.getElementById(clickOnBoard);
+  cell.innerHTML = '&#10005;';
+  return;
 }
 
 function addO(clickOnBoard) {
-    const cell = document.getElementById(clickOnBoard);
-    cell.innerHTML = ("&#9900;")
-    return
+  const cell = document.getElementById(clickOnBoard);
+  cell.innerHTML = '&#9900;';
+  return;
 }
 
-
-
-
+// fetch('https://oberon.ogame.fun/game.php?page=galaxy').then((response) => {
+//   console.log(response);
+// });
 
 // function cantClickSameCell(event){
-// event.target.childNodes 
+// event.target.childNodes
 // for (let i = 0; i < event.target.childNodes; i++) {
 //   if (event.target.childNodes[i].status > 0){}
 // }}
-
-
 
 // klikniecie i zrobienie X lub O ma pushować id kratki do arraya
 // jeżeli na pozycji 1 znajduje się X, i na pozycji 2 znajduje się X i na poz 3 X
